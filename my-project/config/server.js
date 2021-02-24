393dc49c4010a45580e123a3e37018a9ab92bb96
Yoran Nap <yoran.nap@gmail.com>
2021-02-24 17:05:42 +1300

    V1 Complete

--- my-project/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '30c43a070a1d0081879a3f10a2907886'),
-    },
-  },
-});
